8984c94331f9f9ad6a3f899921abd4a9d7c807db
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-04-17 21:59:35 -0700

    got flickity carousel working

--- cocktailSummer/js/main.js
+++ cocktailSummer/js/main.js
@@ -1,59 +1,39 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
 // TODO finish this up and refactor
-document.querySelector('button').addEventListener('click', getDrink)
-document.querySelector('.navigateDrinkLeft').addEventListener('click', scrollLeft)
-document.querySelector('.navigateDrinkRight').addEventListener('click', scrollRight)
+document.querySelector('button').addEventListener('click', getDrink);
 
-
-function getDrink () {
-    // clear page of previous search
-    document.querySelector('.cocktails').innerHTML = ''
-    let drinkSearch = document.querySelector('input').value.replace(/\s+/g, "_")
-    // get new results from API
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkSearch}`).then(res => res.json()).then(data => {
-        for (let i = 0; i < data.drinks.length; i++) {
-            document.querySelector('.cocktails').innerHTML += `
-                <section id="card${i}">
+function getDrink() {
+  // clear page of previous search
+  document.querySelector('.results').innerHTML = '';
+  let drinkSearch = document.querySelector('input').value.replace(/\s+/g, '_');
+  // get new results from API
+  fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkSearch}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      for (let i = 0; i < data.drinks.length; i++) {
+        document.querySelector('.results').innerHTML += `
+                <section class="carousel-cell" id="card${i}">
                     <h2>${data.drinks[i].strDrink}</h2>
                     <img src="${data.drinks[i].strDrinkThumb}" alt="Photo of a delicious ${data.drinks[i].strDrink}">
                     <h3>${data.drinks[i].strInstructions}</h3>
                 </section>
             `;
-        }
-    // console.log(data)
-    document.querySelector('.navigateDrinkLeft').classList.add('hidden')
-    document.querySelector('.navigateDrinkRight').classList.remove('hidden')
-})
+      }
+      // console.log(data)
+    });
+  initFlickity();
 }
-
-function toggleHidden (domItem) {
-    document.querySelector(domItem).classList.toggle('hidden')
-}
-
-let test = 0
-function scrollRight () {
-    if (document.querySelector('.navigateDrinkLeft').classList.contains('hidden')){
-        toggleHidden('.navigateDrinkLeft')
-    }
-    if (test !== 0) {
-        writeArrowLinks(getCurrentLinks().map(element => +element + 1))
-    }
-    test += 1
-}
-function scrollLeft () {
-    if (document.querySelector('.navigateDrinkRight').classList.contains('hidden')){
-        toggleHidden('.navigateDrinkRight')
-    }
-    writeArrowLinks(getCurrentLinks().map(element => +element - 1))
-}
-function getCurrentLinks () {
-    return [document.querySelectorAll('.navigateDrinkLeft')[0].hash.split('').pop(), document.querySelectorAll('.navigateDrinkRight')[0].hash.split('').pop()]
-}
-function writeArrowLinks (arr) {
-    document.querySelector('.navigateDrinkLeft').href = `#card${arr[0]}`
-    document.querySelector('.navigateDrinkRight').href = `#card${arr[1]}`
-}
-
-
-// TODO right arrow is skipping cocktail #2
-// TODO hide right arrow when on last card
+// initialize flickity
+const initFlickity = async () => {
+  const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+  await delay(1000);
+  const elem = document.querySelector('.main-carousel');
+  const flkty = new Flickity(elem, {
+    // options
+    cellAlign: 'center',
+    wrapAround: true,
+    autoplay: 5000,
+  });
+};
