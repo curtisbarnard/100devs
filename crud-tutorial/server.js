bd8e600a6d73e73751ee0b454e554647d9680d3f
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-05-25 16:46:52 -0700

    server up and running

--- crud-tutorial/server.js
+++ crud-tutorial/server.js
@@ -1,1 +1,7 @@
-console.log('Node is doing its thang');
+import express from 'express';
+const app = express();
+
+const port = 8000;
+app.listen(port, function () {
+  console.log(`Node is doing its thang on port ${port}`);
+});
