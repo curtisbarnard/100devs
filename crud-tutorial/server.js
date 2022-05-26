d9ec4a87bb865c0c599acca726c492e86f01f111
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-05-25 18:06:22 -0700

    post to mongodb

--- crud-tutorial/server.js
+++ crud-tutorial/server.js
@@ -1,16 +1,36 @@
 import express from 'express';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+import { connectionString } from './password.js';
+
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const port = 8000;
-app.listen(port, function () {
-  console.log(`Node is doing its thang on port ${port}`);
-});
 
-app.get('/', (request, response) => {
-  // response.send("What's up!");
-  response.sendFile(__dirname + '/index.html');
-});
+MongoClient.connect(connectionString)
+  .then((client) => {
+    console.log('Connected to database');
+    const db = client.db('my-quotes');
+    const quotesCollection = db.collection('quotes');
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.get('/', (request, response) => {
+      response.sendFile(__dirname + '/index.html');
+    });
+
+    app.post('/quotes', (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          console.log(result);
+          res.redirect('/');
+        })
+        .catch((err) => console.error(err));
+    });
+    app.listen(port, function () {
+      console.log(`Node is doing its thang on port ${port}`);
+    });
+  })
+  .catch((err) => console.log(err));
