71bbd7f668b7a3d13ada90867b0a71c20ad98150
Curtis Barnard <curtis.barnard.jr@gmail.com>
2022-05-25 17:06:48 -0700

    serve html file

--- crud-tutorial/server.js
+++ crud-tutorial/server.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const port = 8000;
 app.listen(port, function () {
   console.log(`Node is doing its thang on port ${port}`);
+});
+
+app.get('/', (request, response) => {
+  // response.send("What's up!");
+  response.sendFile(__dirname + '/index.html');
 });
